5abd93f7e520eba1bfb35471103db6ec0b12549d
Tomasz Cekało <tomasz.cekalo@gmail.com>
2021-06-10 11:23:17 +0200

    added live data tests

---
+++ StatLeadersUfcComScraper.Tests/LiveDataTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StatLeadersUfcComScraper.Tests
+{
+    [TestClass]
+    public class LiveDataTests
+    {
+        [TestMethod]
+        public void TestScrapeRoundComb()
+        {
+            IStatLeadersScraper scraper = new StatLeadersScraper();
+            var roundComb = scraper.ScrapeRoundComb();
+        }
+        [TestMethod]
+        public void TestScrapeCareer()
+        {
+            IStatLeadersScraper scraper = new StatLeadersScraper();
+            var roundComb = scraper.ScrapeCareer();
+        }
+        [TestMethod]
+        public void TestScrapeFight()
+        {
+            IStatLeadersScraper scraper = new StatLeadersScraper();
+            var roundComb = scraper.ScrapeFight();
+        }
+        [TestMethod]
+        public void TestScrapeFightComb()
+        {
+            IStatLeadersScraper scraper = new StatLeadersScraper();
+            var roundComb = scraper.ScrapeFightComb();
+        }
+        [TestMethod]
+        public void TestScrapeRound()
+        {
+            IStatLeadersScraper scraper = new StatLeadersScraper();
+            var roundComb = scraper.ScrapeRound();
+        }
+    }
+}
